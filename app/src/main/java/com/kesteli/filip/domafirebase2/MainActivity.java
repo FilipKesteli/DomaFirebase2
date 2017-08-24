826bd848389c22ccdda7f71fd2db3253a4dab595
Filip Kesteli <filip.kesteli@gmail.com>
2017-08-24 06:55:38 +0200

    1.0 - promjena
    proba

--- app/src/main/java/com/kesteli/filip/domafirebase2/MainActivity.java
+++ app/src/main/java/com/kesteli/filip/domafirebase2/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //dank
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
